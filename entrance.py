f6a04a72c7e456261c7d6ce9a3df8e381c5eb903
SonderLau <liuless26@gmail.com>
2021-12-09 22:34:53 +0800

    :passport_control: Refactor: the enter/ run

--- entrance.py
+++ entrance.py
@@ -1,60 +1,78 @@
-from PyInquirer import prompt, Separator
-
-from examples import custom_style_2
+from InquirerPy import inquirer
+from InquirerPy.base.control import Choice
+from InquirerPy.separator import Separator
+from rich.pretty import pprint
+from collections import defaultdict
 from icecream import ic
 import json
+from utils.TimestampCalculate import target_timestamp
+# 整理
+tasks = defaultdict(lambda : [])
 
 with open("./config/subscription.json", "r") as fp:
     questions = json.load(fp)
 
-    pre_set = [
-        {
-            "type": "checkbox",
-            "qmark": "😃",
-            "message": "选择要下载的视频",
-            "name": "Kuaishou Download",
-            "choices": [],
-            "validate": lambda answer: "请至少选择一个视频" if len(answer) == 0 else True,
-        }
-    ]
-
-    choices = pre_set[0]["choices"]
+    choices = []
 
     # json 文件内容解析
     for k, v in questions.items():
-        choices.append(Separator("= {} =".format(k)))
+        choices.append(Separator())
 
         cnt = 0
         for dv in v:
             choices.append(
-                {
-                    "name": dv["name"],
-                    "value": [
-                        dv["value"],
-                        
-                        # ? 所属分类
-                        k,   
-                        
-                        # ? 所在位置
-                        cnt
-                    ],
-                    "checked": dv["checked"]
-                }
+                Choice(
+                    {
+                        "name": dv["name"],
+                        "value": [
+                            dv["value"],
+                            # ? 所属分类
+                            k,
+                            # ? 所在位置
+                            cnt,
+                        ]
+                    },
+                    name=dv["name"],
+                    enabled=dv["checked"],
+                )
             )
             cnt += 1
 
-    pre_set[0]["choices"] = choices
-    answers = prompt(pre_set, style=custom_style_2)
+    answers = inquirer.checkbox(
+        message="😃 请选择要下载的视频",
+        choices=choices,
+        cycle=False,
+        #todo: 分类\总个数统计
+        transformer=lambda result: "%s region%s selected"
+        % (len(result), "s" if len(result) > 1 else ""),
+    ).execute()
 
-    ic(answers)
 
-    # 对选择内容写入文件
 
-    for _ in answers.values():
-        for i in _:
-            questions[i[1]][i[2]]["checked"] = True
-    
-    ic(questions)
-    
-    with open('./config/subscription.json', 'w') as fp:
+    # 全部设置为 False
+    for k,v in questions.items():
+        for one in v:
+            one["checked"] = False
+
+    # 选择的选项设置为 True
+    for _ in answers:
+        category = _["value"][1]
+        index = _["value"][2]
+        questions[category][index]["checked"] = True
+        
+        # 分类
+        tasks[category].append(_)
+
+    # 写入
+    with open("./config/subscription.json", "w") as fp:
         json.dump(questions, fp)
+    
+    
+# 指定下载的时间
+
+print("请输入你想抓取的视频发布起始日期\n", "格式：13d / 1m")
+name = inquirer.text(message="请输入: ").execute()
+
+# 计算目标时间戳
+timestamp = target_timestamp(name)
+
--- run.py
+++ run.py
@@ -1,60 +1,48 @@
 import time
+from typing import List
 
 from GetVideoIndex import get_all_videos
 from MergeVideoFile import merge_video_files
 from ParseDownload import download_short_video
-from Configuration.config import Configuration
+from config.config import Configuration
 from rich.console import Console
 
-console = Console()
-#
-index = {
-    '3x4g2b9attf6q9u': "成杰",
 
-    '3xm4ygx7iisxj5e': "老头",
+def run(category: str, names: List, last_update: str):
+    """运行视频下载
 
-    '3xfjfdnmb78vh3m': "浪人",
+    Args:
+        category (str): 类别
+        names (List): 当前分类下的人
+    """
 
-    '3xwaxvfvcps7awu': "小石",
+    result = {}
 
-    '3xxd79tgpnd5tqy': "多多",
+    # 遍历所有的人 查找符合条件的视频
+    for key in names:
+        result.update(
+            get_all_videos(user_id=key, last_update=last_update, author_name=key)
+        )
 
-    '3xvsnr6zrmvzijc': "浪言",
+    # 对所有的作品进行时间的排序
+    sorted(result, key=lambda x: result[x]["timestamp"])
 
-    '3xfeqrftsqvbisu': "表弟",
+    # 输入文件清空
+    with open(Configuration().get_video_download_path() + "in.md", "w") as fl:
+        fl.write("")
+    # 对所有作品进行下载
+    for key in result:
+        download_short_video(
+            result[key]["download"], result[key]["timestamp"], result[key]["author"]
+        )
 
-    '3xhu9p77zphvbbs': "大表弟",
+    # 合并视频
+    merge_video_files()
 
-    '3x2k7pdreevzzn6': "扛把子"
-}
-
-if __name__ == '__main__':
-
-    # 读取上一次更新的时间
-    with open('./last_update.md', 'r') as f:
-        last_update = f.readlines()[0]
-        result = {}
-
-        # 遍历所有的人 查找符合条件的视频
-        for key in index:
-            result.update(get_all_videos(key, last_update, index[key]))
-
-        # 对所有的作品进行时间的排序
-        sorted(result, key=lambda x: result[x]['timestamp'])
-        console.print(result)
-
-        # 输入文件清空
-        with open(Configuration().get_video_download_path() + 'in.md', 'w') as fl:
-            fl.write('')
-
-        # 对所有作品进行下载
-        for key in result:
-            download_short_video(result[key]['download'], result[key]['timestamp'], result[key]['author'])
-
-        # 合并视频
-        merge_video_files()
-
-        # 写入最后一次更新的时间
-        with open('./last_update_md', 'w') as flush:
-            flush.write(
-                str(int(time.time() * 1000.0)) + '\n\n' + time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime(time.time())))
+    # 写入最后一次更新的时间
+    with open("./last_update_md", "w") as flush:
+        flush.write(
+            str(int(time.time() * 1000.0))
+            + "\n\n"
+            + time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime(time.time()))
+        )
