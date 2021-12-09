096cd4edb2d48f51867ef0f9932aff1094b3269b
SonderLau <liuless26@gmail.com>
2021-12-09 22:37:05 +0800

    :stethoscope: Test: add merged test with subprocess

---
+++ out/DownloadInfo.py
@@ -0,0 +1,29 @@
+import subprocess
+from ffmpy3 import FFmpeg
+from config.config import Configuration
+
+config = Configuration()
+
+prefix = "/mnt/d/repositories/KuaiShouVideoDownload/out/"
+
+cmd = [
+    "ffmpeg -f concat -safe 0 -i "
+    + config.get_merged_video_path()
+    + "in"
+    + config.get_merged_video_path()
+    + "merged.mp4"
+    + " -hide_banner"
+]
+
+process = subprocess.Popen(
+    cmd,
+    shell=True,
+    stdout=subprocess.PIPE,
+    stderr=subprocess.STDOUT,
+    encoding="utf-8",
+    text=True,
+)
+
+# frame=   74 fps= 41 q=29.0 size=     256kB time=00:00:02.79 bitrate= 749.5kbits/s speed=1.55x
+for line in process.stdout:
+    print(line)
