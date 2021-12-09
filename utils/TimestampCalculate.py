0da80d15cd2e64d944586a789e72971a09d8bb7e
SonderLau <liuless26@gmail.com>
2021-12-09 22:33:17 +0800

    :children_crossing: Utils: Timestamp calculator

---
+++ utils/TimestampCalculate.py
@@ -0,0 +1,26 @@
+from InquirerPy import inquirer
+from InquirerPy.base.control import Choice
+from InquirerPy.separator import Separator
+from rich import print
+from collections import defaultdict
+from icecream import ic
+import json
+import time
+
+time_unit = {
+    "d": 86400000,
+    "m": 2678400000
+}
+
+def target_timestamp(raw: str) -> int:
+    # 去空格
+    name = str(raw).rstrip().lstrip()
+
+    mil_seconds = int(name[:-2]) * time_unit[name[-1]]
+
+    # 对 日期进行数位的对齐 取整
+    now = round(time.time() * 1000) 
+
+    return now - mil_seconds
+
+
